dc8ee40041f7d04a056dd482d46beba3f043549d
Andy Coates <8012398+big-andy-coates@users.noreply.github.com>
2022-04-24 12:39:11 +0100

    Add seed, input & expectation base types. (#7)
    * Add seed, input & expectation base types.
    
    Part of: https://github.com/creek-service/creek-system-test/issues/5

--- extension/src/main/java/module-info.java
+++ extension/src/main/java/module-info.java
@@ -1,1 +1,6 @@
-module creek.system.test.extension {}
+module creek.system.test.extension {
+    exports org.creek.api.system.test.extension;
+    exports org.creek.api.system.test.extension.model;
+
+    uses org.creek.api.system.test.extension.CreekTestExtension;
+}
--- extension/src/main/java/org/creek/api/system/test/extension/model/Input.java
+++ extension/src/main/java/org/creek/api/system/test/extension/model/Input.java
@@ -14,21 +14,7 @@
  * limitations under the License.
  */
 
-plugins {
-    `java-library`
-}
+package org.creek.api.system.test.extension.model;
 
-val creekVersion : String by extra
-val jacksonVersion : String by extra
-
-dependencies {
-    api(project(":extension"))
-    api("com.fasterxml.jackson.core:jackson-annotations:${jacksonVersion}")
-
-    implementation("org.creek:creek-base-type:$creekVersion")
-    implementation("com.fasterxml.jackson.core:jackson-core:${jacksonVersion}")
-    implementation("com.fasterxml.jackson.core:jackson-databind:${jacksonVersion}")
-
-    testImplementation(project(":parser"))
-
-}
+/** A test input */
+public interface Input {}
