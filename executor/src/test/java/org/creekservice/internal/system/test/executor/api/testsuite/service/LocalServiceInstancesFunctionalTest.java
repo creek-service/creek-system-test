bdcd2aac45eea63521775894d54141c119b45863
Andy Coates <8012398+big-andy-coates@users.noreply.github.com>
2022-06-12 15:50:25 +0100

    Support service instances having environment variables and exposed ports (#72)
    * Support service instances having environment variables and exposed ports set.
    
    fixes: https://github.com/creek-service/creek-system-test/issues/60
    
    * spotless
    
    * fix test
    
    * fix test

--- executor/src/test/java/org/creekservice/internal/system/test/executor/api/testsuite/service/LocalServiceInstancesFunctionalTest.java
+++ executor/src/test/java/org/creekservice/internal/system/test/executor/api/testsuite/service/LocalServiceInstancesFunctionalTest.java
@@ -16,29 +16,26 @@
 
 package org.creekservice.internal.system.test.executor.api.testsuite.service;
 
+import static java.util.Objects.requireNonNullElse;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.startsWith;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.params.ParameterizedTest.INDEX_PLACEHOLDER;
 import static org.mockito.Mock.Strictness.LENIENT;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.exception.NotFoundException;
-import com.google.common.testing.NullPointerTester;
-import java.lang.reflect.Method;
+import com.github.dockerjava.api.model.ContainerConfig;
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.InternetProtocol;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.ConcurrentModificationException;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import org.creekservice.api.system.test.extension.service.ServiceDefinition;
 import org.creekservice.api.system.test.extension.service.ServiceInstance;
 import org.hamcrest.Description;
@@ -47,9 +44,6 @@ import org.hamcrest.TypeSafeDiagnosingMatcher;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.testcontainers.DockerClientFactory;
@@ -73,14 +67,6 @@ class LocalServiceInstancesFunctionalTest {
     }
 
     @Test
-    void shouldThrowNPEs() {
-        final NullPointerTester tester = new NullPointerTester();
-        tester.testAllPublicConstructors(LocalServiceInstances.class);
-        tester.testAllPublicStaticMethods(LocalServiceInstances.class);
-        tester.testAllPublicInstanceMethods(instances);
-    }
-
-    @Test
     void shouldAddMultipleServiceInstances() {
         // Given:
         final ServiceInstance instance0 = instances.add(serviceDef);
@@ -96,9 +82,10 @@ class LocalServiceInstancesFunctionalTest {
     void shouldNotStartServicesOnAdd() {
         // When:
         final ServiceInstance instance = instances.add(serviceDef);
+        final String instanceId = instanceId(instance);
 
         // Then:
-        assertThat(instance, is(running(false)));
+        assertThat(instance, is(running(false, instanceId)));
     }
 
     @Test
@@ -106,34 +93,38 @@ class LocalServiceInstancesFunctionalTest {
         // Given:
         final ServiceInstance instance0 = instances.add(serviceDef);
         final ServiceInstance instance1 = instances.add(serviceDef);
+        String instanceId0 = instanceId(instance0);
+        String instanceId1 = instanceId(instance1);
 
         // When:
         instance0.start();
 
         // Then:
-        assertThat(instance0, is(running(true)));
-        assertThat(instance1, is(running(false)));
+        assertThat(instance0, is(running(true, instanceId0)));
+        assertThat(instance1, is(running(false, instanceId1)));
+        instanceId0 = instanceId(instance0);
 
         // When:
         instance1.start();
 
         // Then:
-        assertThat(instance0, is(running(true)));
-        assertThat(instance1, is(running(true)));
+        assertThat(instance0, is(running(true, instanceId0)));
+        assertThat(instance1, is(running(true, instanceId1)));
+        instanceId1 = instanceId(instance1);
 
         // When:
         instance0.stop();
 
         // Then:
-        assertThat(instance0, is(running(false)));
-        assertThat(instance1, is(running(true)));
+        assertThat(instance0, is(running(false, instanceId0)));
+        assertThat(instance1, is(running(true, instanceId1)));
 
         // When:
         instance1.stop();
 
         // Then:
-        assertThat(instance0, is(running(false)));
-        assertThat(instance1, is(running(false)));
+        assertThat(instance0, is(running(false, instanceId0)));
+        assertThat(instance1, is(running(false, instanceId1)));
     }
 
     @Test
@@ -154,13 +145,14 @@ class LocalServiceInstancesFunctionalTest {
         // Given:
         final ServiceInstance instance = instances.add(serviceDef);
         instance.start();
-        assertThat(instance, is(running(true)));
+        final String instanceId = instanceId(instance);
+        assertThat(instance, is(running(true, instanceId)));
 
         // When:
         instance.start();
 
         // Then: no error and nothing has changed:
-        assertThat(instance, is(running(true)));
+        assertThat(instance, is(running(true, instanceId)));
     }
 
     @Test
@@ -168,14 +160,15 @@ class LocalServiceInstancesFunctionalTest {
         // Given:
         final ServiceInstance instance = instances.add(serviceDef);
         instance.start();
+        final String instanceId = instanceId(instance);
         instance.stop();
-        assertThat(instance, is(running(false)));
+        assertThat(instance, is(running(false, instanceId)));
 
         // When:
         instance.stop();
 
         // Then: no error and nothing has changed:
-        assertThat(instance, is(running(false)));
+        assertThat(instance, is(running(false, instanceId)));
     }
 
     @Test
@@ -183,13 +176,15 @@ class LocalServiceInstancesFunctionalTest {
         // Given:
         final ServiceInstance instance = instances.add(serviceDef);
         instance.start();
+        final String oldInstanceId = instanceId(instance);
         instance.stop();
 
         // When:
         instance.start();
 
         // Then:
-        assertThat(instance, is(running(true)));
+        assertThat(dockerContainerRunState(oldInstanceId), is(false));
+        assertThat(dockerContainerRunState(instanceId(instance)), is(true));
     }
 
     @Test
@@ -249,29 +244,40 @@ class LocalServiceInstancesFunctionalTest {
                 is("The following services are still running: test-service-1, test-service-2"));
     }
 
-    @SuppressWarnings("unused")
-    @ParameterizedTest(name = "[" + INDEX_PLACEHOLDER + "] {0}")
-    @MethodSource("publicMethods")
-    void shouldThrowIfWrongThread(
-            final String ignored, final Consumer<LocalServiceInstances> method) {
+    @SuppressWarnings("ConstantConditions")
+    @Test
+    void shouldSetEnvOnInstance() {
         // Given:
-        instances = new LocalServiceInstances(Thread.currentThread().getId() + 1);
+        final ServiceInstance instance = instances.add(serviceDef);
+
+        // When:
+        instance.modify().withEnv("CREEK_TEST_ENV_KEY_0", "expected value");
 
         // Then:
-        assertThrows(ConcurrentModificationException.class, () -> method.accept(instances));
+        instance.start();
+        assertThat(instanceConfig(instance).getEnv(), is(notNullValue()));
+        assertThat(
+                List.of(instanceConfig(instance).getEnv()),
+                hasItem("CREEK_TEST_ENV_KEY_0=expected value"));
     }
 
     @Test
-    void shouldHaveThreadingTestForEachPublicMethod() {
-        final List<String> publicMethodNames = publicMethodNames();
-        final int testedMethodCount = (int) publicMethods().count();
+    void shouldSetExposedPortOnInstance() {
+        // Given:
+        final ServiceInstance instance = instances.add(serviceDef);
+
+        // When:
+        instance.modify().withExposedPorts(8080);
+
+        // Then:
+        instance.start();
         assertThat(
-                "Public methods:\n" + String.join(System.lineSeparator(), publicMethodNames),
-                testedMethodCount,
-                is(publicMethodNames.size()));
+                instanceConfig(instance).getExposedPorts(),
+                is(new ExposedPort[] {new ExposedPort(8080, InternetProtocol.TCP)}));
     }
 
-    private Matcher<ServiceInstance> running(final boolean expectedRunState) {
+    private Matcher<ServiceInstance> running(
+            final boolean expectedRunState, final String cachedInstanceId) {
         return new TypeSafeDiagnosingMatcher<>() {
             @Override
             protected boolean matchesSafely(
@@ -286,7 +292,8 @@ class LocalServiceInstancesFunctionalTest {
                             .appendValue(testContainersRunState);
                 }
 
-                final boolean dockerContainerRunState = dockerContainerRunState(item);
+                final boolean dockerContainerRunState =
+                        dockerContainerRunState(item, cachedInstanceId);
                 final boolean dockerContainerStateMatch =
                         dockerContainerRunState == expectedRunState;
                 if (!dockerContainerStateMatch) {
@@ -310,12 +317,22 @@ class LocalServiceInstancesFunctionalTest {
         };
     }
 
-    private boolean dockerContainerRunState(final ServiceInstance instance) {
-        final String containerId = ((InstanceUnderTest) instance).cachedContainerId();
-        if (containerId == null) {
+    private boolean dockerContainerRunState(
+            final ServiceInstance instance, final String cachedInstanceId) {
+        final String containerId = ((InstanceUnderTest) instance).containerId();
+        if (containerId == null && cachedInstanceId == null) {
             return false; // Never started
         }
 
+        if (containerId != null && cachedInstanceId != null) {
+            assertThat(containerId, is(cachedInstanceId));
+        }
+
+        final String id = requireNonNullElse(containerId, cachedInstanceId);
+        return dockerContainerRunState(id);
+    }
+
+    private boolean dockerContainerRunState(final String containerId) {
         try {
             return Boolean.TRUE.equals(
                     dockerClient.inspectContainerCmd(containerId).exec().getState().getRunning());
@@ -324,36 +341,20 @@ class LocalServiceInstancesFunctionalTest {
         }
     }
 
+    private ContainerConfig instanceConfig(final ServiceInstance instance) {
+        return dockerClient
+                .inspectContainerCmd(((InstanceUnderTest) instance).containerId())
+                .exec()
+                .getConfig();
+    }
+
     private static List<ServiceInstance> instances(final LocalServiceInstances instances) {
         final List<ServiceInstance> result = new ArrayList<>(2);
         instances.forEach(result::add);
         return result;
     }
 
-    @SuppressWarnings("unchecked")
-    public static Stream<Arguments> publicMethods() {
-        return Stream.of(
-                Arguments.of(
-                        "clear", (Consumer<LocalServiceInstances>) LocalServiceInstances::clear),
-                Arguments.of(
-                        "spliterator",
-                        (Consumer<LocalServiceInstances>) LocalServiceInstances::spliterator),
-                Arguments.of(
-                        "iterator",
-                        (Consumer<LocalServiceInstances>) LocalServiceInstances::iterator),
-                Arguments.of(
-                        "add",
-                        (Consumer<LocalServiceInstances>)
-                                si -> si.add(mock(ServiceDefinition.class))),
-                Arguments.of(
-                        "forEach",
-                        (Consumer<LocalServiceInstances>) si -> si.forEach(mock(Consumer.class))));
-    }
-
-    private List<String> publicMethodNames() {
-        return Arrays.stream(LocalServiceInstances.class.getMethods())
-                .filter(m -> !m.getDeclaringClass().equals(Object.class))
-                .map(Method::toGenericString)
-                .collect(Collectors.toUnmodifiableList());
+    private static String instanceId(final ServiceInstance instance) {
+        return ((InstanceUnderTest) instance).containerId();
     }
 }
